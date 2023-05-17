950719b05e6ef28ccc27b5e170178e7e16e6d51f
zoftik <ashishnavale25@gmail.com>
2023-05-17 08:26:29 +0530

    1 movie card created

--- ReactJS/Create_react_app/movie-app/src/MovieCard.js
+++ ReactJS/Create_react_app/movie-app/src/MovieCard.js
@@ -1,4 +1,5 @@
 import React from "react"; /// can import directly as component instead of react
+import img from "./assets/avengers.jpeg";
 
 class MovieCard extends React.Component {
   render() {
@@ -6,7 +7,7 @@ class MovieCard extends React.Component {
       <div className="main">
         <div className="movie-card">
           <div className="left">
-            <img alt="poster" />
+            <img alt="poster" src={img} />
           </div>
           <div className="right">
             <div className="title">The Avengers</div>
@@ -15,7 +16,16 @@ class MovieCard extends React.Component {
 
             <div className="footer">
               <div className="rating">8.9</div>
-              <div className="stars">star</div>
+              <div className="star-dis">
+                <img className="str-btn" alt="decrease" src="https://cdn-icons-png.flaticon.com/128/56/56889.png" />
+                <img
+                  alt="star"
+                  src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png"
+                  className="stars"
+                />
+                <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png" />
+                <span>0</span>
+              </div>
               <button className="favourite-btn">Favorite</button>
               <button className="cart-btn">Add To Cart</button>
             </div>
